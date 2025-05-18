79991548a8d15dc456199fa9d9afa4fa05a8f7c0
SergeyGriko <206880@gmail.com>
2025-05-18 08:44:14 +0300

    3

--- src/Main.java
+++ src/Main.java
@@ -24,7 +24,7 @@ public class Main {
         }
         System.out.println();
         System.out.println();
-        System.out.println("Задача Третяя");
+        System.out.println("Задача Третья");
         int startPopulation = 12_000_000;
         int born = startPopulation / 1000 * 17;
         int death = startPopulation / 1000 * 8;
@@ -33,6 +33,18 @@ public class Main {
             totalPopulation += (born - death);
             System.out.println("Год " + i + " численность населения составляет " + totalPopulation);
 
+        }
+        System.out.println();
+        System.out.println("Задача Четвертая");
+        double deposit = 15000.0;
+        double percent = 0.07;
+        double mission = 12_000_000.0;
+        int monts = 0;
+
+        while (deposit < mission) {
+            deposit = deposit + deposit * percent;
+            months++;
+            System.out.println("Сумма накопления за " + monts + " месяц " + deposit + " рублей");
         }
     }
 }
