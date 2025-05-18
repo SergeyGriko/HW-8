b054337f4b4009e61dde4266b2a9bbb5201269bc
SergeyGriko <206880@gmail.com>
2025-05-18 07:50:14 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Задача Первая");
+        int salary = 15000;
+        int target = 2459000;
+        int total = 0;
+        int months = 0;
+        while (total <= target) {
+            total += salary;
+            months++;
+        }
+        System.out.println("Понадобится месяцев: " + months);
+
+        System.out.println();
+        System.out.println("Задача Вторая");
+        int i = 0;
+        while (i < 10) {
+            i++;
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (; i > 0; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        System.out.println();
+        System.out.println("Задача Третяя");
+        int startPopulation = 12_000_000;
+        int born = startPopulation / 1000 * 17;
+        int death = startPopulation / 1000 * 8;
+        int totalPopulation = startPopulation + born + death;
+        for (i = 1; i <= 10; i++) {
+            totalPopulation += (born - death);
+            System.out.println("Год " + i + " численность населения составляет " + totalPopulation);
+
+        }
     }
 }
